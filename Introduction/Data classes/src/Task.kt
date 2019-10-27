ef691ac7814f94ce45f3fd82e7c510aa6f46e128
Grig Alex <alexengrigdev@gmail.com>
2019-10-27 14:20:26 +0300

    Resolves 1 level

--- Introduction/Data classes/src/Task.kt
+++ Introduction/Data classes/src/Task.kt
@@ -1,4 +1,8 @@
-class Person
+class Person(private val name: String, private val age: Int){
+    override fun toString(): String {
+        return "Person(name=$name, age=$age)"
+    }
+}
 
 fun getPeople(): List<Person> {
     return listOf(Person("Alice", 29), Person("Bob", 31))
